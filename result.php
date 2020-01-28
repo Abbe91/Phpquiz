fbdc89751b8fa935f9f729152c32121f2c4f95e5
Abbe91 <abdullah.abo.alhaijja@hotmail.com>
2020-01-28 14:00:11 +0100

    I change my mind and I will make it with foreach

--- result.php
+++ result.php
@@ -1,7 +1,7 @@
 <html>
  
 <head>
-	<meta charset=UTF-8" />
+	<meta charset="UTF-8" />
 	
 	<title>PHP Quiz</title>
 	
@@ -12,22 +12,59 @@
  
 	<div id="page-wrap">
  
-		<h1>Result | Webdevtrick.com</h1>
+		<h1>Result</h1>
 		
         <?php
             
             $answer1 = $_POST['question-1-answers'];
             $answer2 = $_POST['question-2-answers'];
-            $answer3 = $_POST['question-3-answers'];
-            $answer4 = $_POST['question-4-answers'];
+            // $answer3 = $_POST['question-3-answers'];
+            // $answer4 = $_POST['question-4-answers'];
+
+     
         
         
             $totalCorrect = 0;
             
-            if ($answer1 == "D") { $totalCorrect++; }
-            if ($answer2 == "C") { $totalCorrect++; }
-            if ($answer3 == "B") { $totalCorrect++; }
-            if ($answer4 == "B") { $totalCorrect++; }
+            if ($answer1 == "D")
+             {
+            $totalCorrect++; 
+            echo "Hur länge finns något i GLOBALS variabel i Php? </br>";
+            echo  "Rätt </br>" ; 
+            }else{
+            echo "Hur länge finns något i GLOBALS variabel i Php? </br>";
+            echo  "Fell </br>" ; 
+            }
+            if ($answer2 == "C") 
+            {
+            $totalCorrect++;
+            echo "Vad är _POST och _GET ? </br>";
+            echo  " Rätt </br>";
+            }else{
+            echo "Vad är _POST och _GET ? </br>";
+            echo  "Fell </br>" ; 
+            }
+            
+            // if ($answer3 == "B")
+            // {
+            // $totalCorrect++;
+            // echo "PHP is a.... </br>";
+            // echo " Rätt </br>";
+            // }else{
+            // echo "PHP is a.... </br>";
+            // echo  "Fell </br>" ; 
+            // }
+           
+            // if ($answer4 == "B")
+            // { 
+            // $totalCorrect++; 
+            // echo "Vad är en cookie ?</br>";
+            // echo  " Rätt </br>";
+            // }else{
+            // echo "Vad är en cookie ?</br>";
+            // echo  "Fell </br>" ; 
+            // }
+            
            
             
             echo "<div id='results'>$totalCorrect / 4 correct</div>";
